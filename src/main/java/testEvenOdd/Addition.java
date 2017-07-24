5dff71df2d58b835ab555b547040df9c65eefbfc
VIDYA <vidya.kaspar822gmail.com>
2017-07-24 07:37:14 -0400

    Checking

--- src/main/java/testEvenOdd/Addition.java
+++ src/main/java/testEvenOdd/Addition.java
@@ -2,6 +2,6 @@ package testEvenOdd;
 
 public class Addition {
 	 public String add(){
-         return ("Hello World Changed for Testing!!!");
+         return ("Hello World Changed for Testing Now!!!");
    }
 }
